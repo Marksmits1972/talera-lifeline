1f18144411201f63835022aec8b21019ac8b6428
Marksmits1972 <mark@swimm.nl>
2026-09-08 15:32:04 +0200

    Wire complete TALERA Worker source

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import { chunk1 } from "./html/chunk1.js";
 import { chunk2 } from "./html/chunk2.js";
 import { chunk3 } from "./html/chunk3.js";
 import { chunk4 } from "./html/chunk4.js";
+import { chunk5 } from "./html/chunk5.js";
+import { chunk6 } from "./html/chunk6.js";
+import { chunk7 } from "./html/chunk7.js";
+import { chunk8 } from "./html/chunk8.js";
+import { chunk9 } from "./html/chunk9.js";
 
-const HTML = [...chunk1, ...chunk2, ...chunk3, ...chunk4].join("\n");
+const HTML = [
+  ...chunk1,
+  ...chunk2,
+  ...chunk3,
+  ...chunk4,
+  ...chunk5,
+  ...chunk6,
+  ...chunk7,
+  ...chunk8,
+  ...chunk9,
+].join("\n");
 
 export default {
   async fetch() {

cbb878f3daf5b27801d846d83d9790c7542eebb2
Marksmits1972 <mark@swimm.nl>
2026-09-09 15:42:48 +0200

    Restore presentation worker separation from story page

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import { chunk9 } from "./html/chunk9.js";
 import { enhancementStyle, enhancementScript } from "./enhancement.js";
 import { interactionFixStyle, interactionFixScript } from "./interaction-fixes.js";
 import { timelineTransitionStyle } from "./timeline-transitions.js";
-import { storyPageStyle, storyPageScript } from "./story-page.js";
 
 const BASE_HTML = [
   ...chunk1,
@@ -25,8 +24,8 @@ const BASE_HTML = [
 ].join("\n");
 
 const HTML = BASE_HTML
-  .replace("</head>", `<style id="talera-immersive-photo">${enhancementStyle}</style><style id="talera-interaction-fixes">${interactionFixStyle}</style><style id="talera-timeline-transitions">${timelineTransitionStyle}</style><style id="talera-story-page">${storyPageStyle}</style></head>`)
-  .replace("</body>", `<script id="talera-immersive-photo-script">${enhancementScript}</script><script id="talera-interaction-fixes-script">${interactionFixScript}</script><script id="talera-story-page-script">${storyPageScript}</script></body>`);
+  .replace("</head>", `<style id="talera-immersive-photo">${enhancementStyle}</style><style id="talera-interaction-fixes">${interactionFixStyle}</style><style id="talera-timeline-transitions">${timelineTransitionStyle}</style></head>`)
+  .replace("</body>", `<script id="talera-immersive-photo-script">${enhancementScript}</script><script id="talera-interaction-fixes-script">${interactionFixScript}</script></body>`);
 
 export default {
   async fetch() {

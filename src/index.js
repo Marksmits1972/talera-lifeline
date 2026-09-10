3610f8240b6f41b83495ba8fa2490b5657d2e045
Marksmits1972 <mark@swimm.nl>
2026-09-10 13:54:23 +0200

    Consolidate presentation touch handling into one controller

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { chunk6 } from "./html/chunk6.js";
 import { chunk7 } from "./html/chunk7.js";
 import { chunk8 } from "./html/chunk8.js";
 import { chunk9 } from "./html/chunk9.js";
-import { enhancementStyle, enhancementScript } from "./enhancement.js";
-import { interactionFixStyle, interactionFixScript } from "./interaction-fixes.js";
-import { swipeHotfixScript } from "./swipe-hotfix.js";
+import { enhancementStyle } from "./enhancement.js";
+import { interactionFixStyle } from "./interaction-fixes.js";
+import { presentationControllerScript } from "./presentation-controller.js";
 
 const BASE_HTML = [
   ...chunk1,
@@ -25,7 +25,7 @@ const BASE_HTML = [
 
 const HTML = BASE_HTML
   .replace("</head>", `<style id="talera-immersive-photo">${enhancementStyle}</style><style id="talera-interaction-fixes">${interactionFixStyle}</style></head>`)
-  .replace("</body>", `<script id="talera-immersive-photo-script">${enhancementScript}</script><script id="talera-interaction-fixes-script">${interactionFixScript}</script><script id="talera-swipe-hotfix-script">${swipeHotfixScript}</script></body>`);
+  .replace("</body>", `<script id="talera-presentation-controller">${presentationControllerScript}</script></body>`);
 
 export default {
   async fetch() {

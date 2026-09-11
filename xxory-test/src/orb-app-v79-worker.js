10a7a838224baaaeedfa582c22327d9812aa937c
Marksmits1972 <mark@swimm.nl>
2026-09-11 18:18:48 +0200

    Wire workblad return navigation into live tell page

--- xxory-test/src/orb-app-v79-worker.js
+++ xxory-test/src/orb-app-v79-worker.js
@@ -3,14 +3,15 @@ import { WORKBLAD_V1_STYLE } from "./workblad-v1-style.js";
 import { WORKBLAD_V1_FOCUS_RING_STYLE } from "./workblad-v1-focus-ring-style.js";
 import { WORKBLAD_V2_STYLE } from "./workblad-v2-style.js";
 import { WORKBLAD_V2_SCRIPT } from "./workblad-v2-client.js";
+import { WORKBLAD_RETURN_STYLE, WORKBLAD_RETURN_SCRIPT } from "./workblad-return-nav.js";
 import { handleWorkbladIntegrationApi } from "./workblad-integration-api.js";
 
-const TALERA_DEPLOY_REV = "workblad-v2-linked-timeline-proxy-ios-stable";
+const TALERA_DEPLOY_REV = "workblad-v2-return-to-timeline";
 
 function enhanceWorkblad(html){
   return html
-    .replace('</head>','<style>'+WORKBLAD_V1_STYLE+WORKBLAD_V1_FOCUS_RING_STYLE+WORKBLAD_V2_STYLE+'</style></head>')
-    .replace('</body>',WORKBLAD_V2_SCRIPT+'</body>');
+    .replace('</head>','<style>'+WORKBLAD_V1_STYLE+WORKBLAD_V1_FOCUS_RING_STYLE+WORKBLAD_V2_STYLE+WORKBLAD_RETURN_STYLE+'</style></head>')
+    .replace('</body>',WORKBLAD_V2_SCRIPT+WORKBLAD_RETURN_SCRIPT+'</body>');
 }
 
 export default {

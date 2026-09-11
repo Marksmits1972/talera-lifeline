d52617ce4d9bf008cd11e140142b3679a2bd1e5d
Marksmits1972 <mark@swimm.nl>
2026-09-11 18:37:08 +0200

    Deploy universal workblad navigation and presentation controls

--- xxory-test/src/orb-app-v79-worker.js
+++ xxory-test/src/orb-app-v79-worker.js
@@ -6,7 +6,7 @@ import { WORKBLAD_V2_SCRIPT } from "./workblad-v2-client.js";
 import { WORKBLAD_UNIVERSAL_NAV_STYLE, WORKBLAD_UNIVERSAL_NAV_SCRIPT } from "./workblad-universal-nav.js";
 import { handleWorkbladIntegrationApi } from "./workblad-integration-api.js";
 
-const TALERA_DEPLOY_REV = "workblad-v2-universal-bottom-nav";
+const TALERA_DEPLOY_REV = "workblad-v2-universal-bottom-nav-v2";
 
 function enhanceWorkblad(html){
   return html

e3499a1ced8231a9a2d572ff98ee7156b4a53045
Marksmits1972 <mark@swimm.nl>
2026-09-12 09:53:19 +0200

    Trigger recorder control core fix

--- xxory-test/src/orb-app-v79-worker.js
+++ xxory-test/src/orb-app-v79-worker.js
@@ -7,7 +7,7 @@ import { WORKBLAD_UNIVERSAL_NAV_STYLE, WORKBLAD_UNIVERSAL_NAV_SCRIPT } from "./w
 import { WORKBLAD_LAYOUT_TUNING_STYLE, WORKBLAD_LAYOUT_TUNING_SCRIPT } from "./workblad-layout-tuning.js";
 import { handleWorkbladIntegrationApi } from "./workblad-integration-api.js";
 
-const TALERA_DEPLOY_REV = "full-listen-cycle-v5-keyboard-finish-visible-20260912";
+const TALERA_DEPLOY_REV = "full-listen-cycle-v6-recorder-controls-core-20260912";
 
 function enhanceWorkblad(html){
   return html
